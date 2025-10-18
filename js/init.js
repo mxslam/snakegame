9f4027e65127f37456038cd31519dda688233892
M-slam <mxslam@protonmail.com>
2025-10-18 14:54:14 +0200

    maj

--- js/init.js
+++ js/init.js
@@ -1,20 +1,9 @@
 const game = new Game();
 
-let baseSpeed = 5; // vitesse nominale
-let timeout = 16; // 16 ms ≈ 60 fps
-let lastTime = performance.now();
+let speed = 5; // px par tick
+let timeout = 100; // 100 ms ≈ 10 FPS
 
 setInterval(() => {
-  const now = performance.now();
-  const elapsed = now - lastTime; // combien de ms se sont écoulées en vrai
-  lastTime = now;
-
-  // ratio entre temps réel et temps prévu
-  const ratio = elapsed / timeout;
-
-  // ajuste la vitesse si une frame est en retard ou en avance
-  speed = baseSpeed * ratio;
-
   game.update();
 }, timeout);
 
